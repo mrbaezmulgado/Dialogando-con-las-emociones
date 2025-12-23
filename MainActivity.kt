118dc334a91f4f4f8a3d2b1e709379a9e0cf005e
mrbaezmulgado <miguelricardobaez@gmail.com>
2025-12-22 21:37:49 -0600

    Update MainActivity.kt
    Se suplió el código, para introducir uno con función de conectar pantallas 
    
    Signed-off-by: mrbaezmulgado <miguelricardobaez@gmail.com>

--- MainActivity.kt
+++ MainActivity.kt
@@ -3,11 +3,6 @@ package com.dialogandoemocion
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.*
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.NavHostController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,35 +10,5 @@ class MainActivity : ComponentActivity() {
         setContent {
             DialogandoEmocionApp()
         }
-    }
-}
-
-@Composable
-fun DialogandoEmocionApp() {
-    val navController = rememberNavController()
-
-    // Memoria: cuántas veces se repite una emoción
-    val emocionRepetidaCount = remember { mutableStateOf(0) }
-    val ultimaEmocion = remember { mutableStateOf("") }
-
-    NavHost(navController = navController, startDestination = "inicio") {
-
-        composable("inicio") { PantallaInicio(navController) }
-
-        composable("dialogo") {
-            PantallaDialogo(
-                navController = navController,
-                emocionRepetidaCount = emocionRepetidaCount,
-                ultimaEmocion = ultimaEmocion
-            )
-        }
-
-        composable("info_emocion") {
-            PantallaInfoEmocion(navController, ultimaEmocion.value)
-        }
-
-        composable("comentario") { PantallaComentario(navController) }
-
-        composable("dibujo") { PantallaDibujo(navController) }
     }
 }
